d2c3529cca229626edd36c411826ca14cab05b1e
David PETROVIC <petrovic.david@outlook.fr>
2019-04-02 10:37:05 +0200

    Upload Base de données
    Upload du fichier SQL de génération de la base de données et changement de nom de cette dernière en "DamirDB".

---
+++ Ressources/DB/DamirDB.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le :  mar. 02 avr. 2019 à 08:33
+-- Version du serveur :  5.7.25-log
+-- Version de PHP :  7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `DamirDB`
+--
+CREATE DATABASE IF NOT EXISTS `DamirDB` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `DamirDB`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `confirmation_token` varchar(255) DEFAULT NULL,
+  `confirmed_at` date DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_at` date DEFAULT NULL,
+  `remember_token` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Ressources/php/inc/db.php
+++ Ressources/php/inc/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $pdo= new PDO('mysql:host=localhost;dbname=arborea','root', 'root');
+    $pdo= new PDO('mysql:host=localhost;dbname=DamirDB','root', 'root');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
