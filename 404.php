87c18a7dd4554e8c60d64eed78768022afe51227
hamza <fekraouihamza@yahoo.fr>
2019-04-10 10:09:52 +0200

    Modif
    j'a modifie la page 404

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@
 			<div class="container text-center">
 				<i class="fas fa-wrench fa-10x"></i>
 				<div><h1>&times;404&times;</h1>
-					<h2>Cette page est actuellement indisponible</h2>
+					<h2>Cette page est actuellement indisponible hhhhhhhhh</h2>
 					<h4>Revenez bient&ocirc;t !</h4>
 				</div>
 				<a class="btn btn-lg" href="index.php" title="Retourner à l'accueil"><i class="fas fa-arrow-left"></i> Retourner &agrave; l'accueil</a><br><br><br>
