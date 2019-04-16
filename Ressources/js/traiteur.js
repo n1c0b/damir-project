402efb6edc523cec0e2f0d759be6bdde29c16ff1
David PETROVIC <petrovic.david@outlook.fr>
2019-04-16 13:35:53 +0200

    Update traiteur et contact
    Rajout d'une image de fond sur la page contact.php
    Update de la page traiteur.php

--- Ressources/js/traiteur.js
+++ Ressources/js/traiteur.js
@@ -1,14 +1,33 @@
 $(function(){
 
+
+    var open = false;
     $('#animationsIMG').click(function(){
-        $('#animation').show();
-        $(this).animate({right: '90%'});
-        $('#forfaitIMG').animate({left: '90%'});
-    });
+        if(open == false){
+            open = true;
+            $('#animation').show();
+            $(this).animate({right: '100%'});
+            $('#forfaitIMG').animate({left: '90%'});
+        } else {
+            $('#animation').show();
+            $('#forfait').hide();
+            $(this).animate({right: '100%'});
+            $('#forfaitIMG').animate({left: '90%'});
+        }
+        });
+
     $('#forfaitIMG').click(function(){
-        $('#forfait').show();
-        $(this).animate({left: '90%'});
-        $('#animationsIMG').animate({right: '90%'});
+        if(open == false){
+            open = true;
+            $('#forfait').show();
+            $(this).animate({left: '100%'});
+            $('#animationsIMG').animate({right: '90%'});
+        } else {
+            $('#forfait').show();
+            $('#animation').hide();
+            $(this).animate({left: '100%'});
+            $('#animationsIMG').animate({right: '90%'});
+        }
     });
 
 });
