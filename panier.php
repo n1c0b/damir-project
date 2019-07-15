7255a8944f291e30b0ff7034b7d571c27fd392b8
David PETROVIC <petrovic.david@outlook.fr>
2019-07-15 08:09:21 +0200

    mise en page

--- panier.php
+++ panier.php
@@ -28,16 +28,23 @@ var_dump($ids); ?>
         <form action="panier.php" method="post">
             <!-- Puis on affiche l'etat actuel du panier avec les id récuperés dans $SESSION['panier'] couplés aux infos de la bdd -->
             <?php foreach ($products as $product) : ?>
-                <div class="col-md-4" style="border: 1px solid black">
-                    <p><img src="Ressources/img/<?= $product->image ?>" alt="" style="width:200px;height:200px;"></p>
-                    <span class="quantity">Nombre d'articles:<input type="text" name="panier[quantity][<?= $product->id; ?>]" value="<?= $_SESSION['panier'][$product->id]; ?>"> </span>
-                    <p><?= $product->name; ?> </p>
-                    <p> <?= number_format($product->prix, 2, '.', '') ?> € </p>
-
-                    <p>
-                        <!-- Se bouton renvoie un $GET['del'] à la page elle même qui sera traité en haut -->
-                        <a href="panier.php?del=<?= $product->id ?>"> supprimer </a>
-                    </p>
+                <div class="col-md-4 cardProd">
+                    <div class="card shadow bgr">
+                        <div class="carditem">
+                            <div class="text-center">
+                                <img class="card img-top img-fluid imgsize" src="Ressources/img/<?= $product->image ?>" alt="">
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <span class="quantity">Nombre d'articles:<input type="text" name="panier[quantity][<?= $product->id; ?>]" value="<?= $_SESSION['panier'][$product->id]; ?>"> </span>
+                            <p><?= $product->name; ?> </p>
+                            <p><?= number_format($product->prix, 2, '.', '') ?> € </p>
+                            <p>
+                                <!-- Se bouton renvoie un $GET['del'] à la page elle même qui sera traité en haut -->
+                                <a href="panier.php?del=<?= $product->id ?>"> Supprimer </a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             <?php endforeach ?>
             <p>Total: <?= number_format($panier->total(), 2, ',', ' ') ?></p>
